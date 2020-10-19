4b9bed3621a7ca507c01b252f5a22c5ed2eb7e2f
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-19 11:36:50 +0530

    Export modified

--- src/store/states/account-state/actions/index.ts
+++ src/store/states/account-state/actions/index.ts
@@ -1,5 +1,6 @@
 import updateAccountAsync from "./update-account-action";
 import loginAsync from "./login-action";
 import logoutAsync from "./logout-action";
+import createAccountAsync from './create-account-action'
 
-export { updateAccountAsync, loginAsync, logoutAsync }
+export { updateAccountAsync, loginAsync, logoutAsync, createAccountAsync }
