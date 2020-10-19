a8de6f3bcd4cc2ded5a0b1e1939e563870aa4925
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-19 11:39:10 +0530

    fixed export

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,5 +1,6 @@
 import login from "./login-api";
 import logout from "./logout-api";
 import status from "./status-api";
+import register from './register-api'
 
-export { login, logout, status }
+export { login, logout, status, register }
