0862de15f844d40faff94e656df03c1899b0a4e1
darkfusion90 <gauravsharmaa341@gmail.com>
2020-10-19 11:37:08 +0530

    Will updated account status on registration

--- src/store/states/account-state/middlewares/monitor-login.ts
+++ src/store/states/account-state/middlewares/monitor-login.ts
@@ -1,9 +1,13 @@
-import { loginAsync, logoutAsync, updateAccountAsync } from '../actions'
+import { loginAsync, logoutAsync, updateAccountAsync, createAccountAsync } from '../actions'
 import { asyncMatchFulfilled } from "../../../utils";
 import { ThunkMiddleware } from "../../../@types/thunk-middleware";
 
 const monitorLoginMiddleware: ThunkMiddleware = (api) => (next) => (action) => {
-    if (asyncMatchFulfilled(loginAsync, action) || asyncMatchFulfilled(logoutAsync, action)) {
+    const hasLoggedIn = asyncMatchFulfilled(loginAsync, action)
+    const hasLoggedOut = asyncMatchFulfilled(logoutAsync, action)
+    const hasRegistered = asyncMatchFulfilled(createAccountAsync, action)
+
+    if (hasLoggedIn || hasLoggedOut || hasRegistered) {
         api.dispatch(updateAccountAsync())
     }
 
