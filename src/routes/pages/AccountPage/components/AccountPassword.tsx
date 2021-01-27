62522fc9fe5bd6eebfed42c737152c1b08be031a
darkfusion90 <gauravsharmaa341@gmail.com>
2021-01-27 18:00:45 +0530

    Moved password reset token generation logic to another component

--- src/routes/pages/AccountPage/components/AccountPassword.tsx
+++ src/routes/pages/AccountPage/components/AccountPassword.tsx
@@ -3,51 +3,25 @@ import Button from '@material-ui/core/Button'
 
 import { SettingCard } from './SettingCard'
 import { IAccountPassword } from '../typings/AccountPassword'
-import useAsyncState from '../../../../hooks/useAsyncState'
-import useAccount from '../../../../hooks/useAccount'
-import { requestPasswordResetToken } from '../../../../api/auth'
-import { Redirect } from 'react-router-dom'
-import { useSnackbar } from 'notistack'
+import { Link } from 'react-router-dom'
+import { routeMap } from '../../../routes'
 
 export const AccountPassword: IAccountPassword = ({ emailVerified }) => {
     return (
         <SettingCard
             title='Password'
             subtitle={emailVerified ? '' : 'Verify your email to reset password'}
-            action={<PasswordResetButton emailVerified={emailVerified} />}
+            action={
+                <Button
+                    variant='contained'
+                    color='primary'
+                    disabled={!emailVerified}
+                    component={Link}
+                    to={routeMap.initiatePasswordReset.path}
+                >
+                    Reset Password
+                </Button>
+            }
         />
-    )
-}
-
-const PasswordResetButton: React.FC<{ emailVerified: boolean }> = ({ emailVerified }) => {
-    const [tokenGenState, tokenGenStateActions] = useAsyncState()
-    const [failSnackbarShown, setFailSnackbarShown] = React.useState(false)
-    const { enqueueSnackbar } = useSnackbar()
-
-    const { user } = useAccount()[0]
-    if (!user) {
-        return null
-    }
-
-    if (tokenGenState.status === 'success') {
-        return <Redirect to='password-reset/generate/success' />
-    }
-
-    if (tokenGenState.status === 'fail' && !failSnackbarShown) {
-        setFailSnackbarShown(true)
-        enqueueSnackbar('Failed to generate password reset token', { variant: 'error' })
-    }
-
-    const genResetToken = () => {
-        tokenGenStateActions.setLoading()
-        requestPasswordResetToken(user.username)
-            .then(_ => tokenGenStateActions.setSuccess())
-            .catch(tokenGenStateActions.setFailure)
-    }
-
-    return (
-        <Button variant='contained' color='primary' disabled={!emailVerified} onClick={genResetToken}>
-            Reset Password
-        </Button>
     )
 }
